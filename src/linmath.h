24f9711a395b32ea145771d8109f7e8b6e7567cb
iubieta <ikerp.ubieta@gmail.com>
2025-08-29 14:10:04 +0200

    tuple_to_array, set, point, vector

---
+++ src/linmath.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   linmath.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iubieta- <iubieta@student.42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/29 12:45:05 by iubieta-          #+#    #+#             */
+/*   Updated: 2025/08/29 13:49:37 by iubieta-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LINMATH_H
+# define LINMATH_H
+
+typedef struct s_tuple {
+	float x;
+	float y;
+	float z;
+	float w;
+}	t_tuple;
+
+float	*tuple_to_array(t_tuple t, float ar[4]);
+t_tuple	tuple_set(float x, float y, float z, float w);
+t_tuple	tuple_point(float x, float y, float z);
+t_tuple	tuple_vector(float x, float y, float z);
+t_tuple	tuple_add(t_tuple a, t_tuple b);
+t_tuple	tuple_sub(t_tuple a, t_tuple b);
+t_tuple	tuple_scale(t_tuple v, float n);
+t_tuple	tuple_negate(t_tuple v);
+t_tuple	tuple_magnitude(t_tuple v);
+t_tuple	tuple_normalize(t_tuple v);
+t_tuple	tuple_dot(t_tuple a, t_tuple b);
+t_tuple	tuple_cross(t_tuple a, t_tuple b);
+
+#endif 
---
+++ src/tuples.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tuples.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iubieta- <iubieta@student.42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/29 13:16:57 by iubieta-          #+#    #+#             */
+/*   Updated: 2025/08/29 13:46:58 by iubieta-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "linmath.h"
+
+float	*tuple_to_array(t_tuple t, float ar[4])
+{
+	ar[0] = t.x;
+	ar[1] = t.y;
+	ar[2] = t.z;
+	ar[3] = t.w;
+	return ar;
+}
+
+t_tuple	tuple_set(float x, float y, float z, float w)
+{
+	t_tuple	t;
+
+	t.x = x;
+	t.y = y;
+	t.z = z;
+	t.w = w;
+	return t;
+}
+
+t_tuple	tuple_point(float x, float y, float z)
+{
+	t_tuple	t;
+
+	t.x = x;
+	t.y = y;
+	t.z = z;
+	t.w = 1;
+	return t;
+}
+	
+// t_tuple	tuple_vector(float x, float y, float z);
+// t_tuple	tuple_add(t_tuple a, t_tuple b);
+// t_tuple	tuple_sub(t_tuple a, t_tuple b);
+// t_tuple	tuple_scale(t_tuple v, float n);
+// t_tuple	tuple_negate(t_tuple v);
+// t_tuple	tuple_magnitude(t_tuple v);
+// t_tuple	tuple_normalize(t_tuple v);
+// t_tuple	tuple_dot(t_tuple a, t_tuple b);
+// t_tuple	tuple_cross(t_tuple a, t_tuple b);
